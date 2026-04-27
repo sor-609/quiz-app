6bdbdc6641c1264bf9287728b05b690c4b7311e4
sor-609 <sora.p0609@gmail.com>
2026-04-27 23:22:53 +0900

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const q = {
-    text: "日本一高い山は何？",
-    correct: 2
-};
+const quizzes = [
+    { question: "日本で一番高い山は何？", answer: "富士山"},
+    { question: "では、富士山がある都道府県はどことどこ？", answer: "山梨県と静岡県"}
+];
 
-document.getElementById("question").textContent = q.text;
+const container = document.getElementById("quiz-list");
 
-function answer(n) {
-    if (n === q.correct) {
-        alert("正解！");
-    } else {
-        alert("不正解");
-    }
-}
+quizzes.forEach((q, index) => {
+    const div = document.createElement("div");
+
+    const p = document.createElement("p");
+    p.textContent = `Q${index + 1}: ${q.question}`;
+
+    const btn = document.createElement("button");
+    btn.textContent = "答えを見る";
+    btn.onclick = () => alert(q.answer);
+
+    div.appendChild(p);
+    div.appendChild(btn);
+    container.appendChild(p);
+});
