dd63d78c70df91718109a1603f7ced3b69988b8a
sor-609 <sora.p0609@gmail.com>
2026-04-28 17:10:47 +0900

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const quizzes = [
-    { question: "日本で一番高い山は何？", answer: "富士山"},
-    { question: "では、富士山がある都道府県はどことどこ？", answer: "山梨県と静岡県"}
+    {
+        question: "日本で一番高い山は何？",
+        choices: ["エベレスト", "北岳", "阿蘇山", "富士山"],
+        answer: "富士山"
+    },
+    {
+        question: "では、富士山がある都道府県はどことどこ？",
+        choices: ["静岡県と愛知県", "山梨県と静岡県", "長野県と山梨県", "長野県と静岡県"],
+        answer: "山梨県と静岡県"
+    }
 ];
 
 const container = document.getElementById("quiz-list");
@@ -8,14 +16,26 @@ const container = document.getElementById("quiz-list");
 quizzes.forEach((q, index) => {
     const div = document.createElement("div");
 
+    // 問題文
     const p = document.createElement("p");
     p.textContent = `Q${index + 1}: ${q.question}`;
-
-    const btn = document.createElement("button");
-    btn.textContent = "答えを見る";
-    btn.onclick = () => alert(q.answer);
-
     div.appendChild(p);
-    div.appendChild(btn);
-    container.appendChild(p);
+
+    // 選択肢ボタン
+    q.choices.forEach(choice => {
+        const btn = document.createElement("button");
+        btn.textContent = choice;
+
+        btn.onclick = () => {
+            if (choice === q.correct) {
+                btn.style.backgroundColor = "lightgreen";
+            } else {
+                btn.style.backgroundColor = "salmon";
+            }
+        };
+
+        div.appendChild(btn);
+    });
+
+    container.appendChild(div);
 });
